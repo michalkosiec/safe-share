33464395e91e6ae39d855485f03dde9ebee33430
Michal Kosiec <michalkosiecg@gmail.com>
2026-08-23 21:19:39 +0200

    Fix button color contrast

--- safe-share-frontend/src/pages/Login.tsx
+++ safe-share-frontend/src/pages/Login.tsx
@@ -40,27 +40,27 @@ export default function Login() {
         console.log("Login clicked!")
     }
     return <div ref={vantaRef} className="min-h-screen flex justify-center items-center">
-        <div className="flex flex-col w-160 p-6 bg-[rgba(255,255,255,0.4)] rounded-xl backdrop-blur-md border border-white/10 shadow-2xl">
+        <div className="flex flex-col w-160 p-6 bg-[rgba(0,0,0,0.6)] rounded-xl backdrop-blur-xl border border-black/10 shadow-2xl text-white">
             <div>
                 <h1 className="text-3xl font-bold tracking-wider">Log in</h1>
             </div>
             <form onSubmit={handleSubmit} className="flex flex-col h-full space-y-6 mt-5">
                 <div>
                     <label>User name</label>
-                    <div className="flex justify-between mt-2 bg-[rgba(255,255,255,0.6)] h-10 rounded-lg items-center pl-2 gap-4">
+                    <div className="flex justify-between mt-2 bg-[rgba(255,255,255,0.1)] h-10 rounded-lg items-center pl-2 gap-4">
                         <User />
                         <input type="text" placeholder="Your login" className="w-full"/>
                     </div>
                 </div>
                 <div>
                     <label>Password</label>
-                    <div className="flex justify-between mt-2 bg-[rgba(255,255,255,0.6)] h-10 rounded-lg items-center pl-2 gap-4">
+                    <div className="flex justify-between mt-2 bg-[rgba(255,255,255,0.1)] h-10 rounded-lg items-center pl-2 gap-4">
                         <Lock />
                         <input type="password" placeholder="Your password" className="w-full"/>
                     </div>
                 </div>
                 <div>
-                    <button type="submit" className="w-full rounded-lg text-white bg-black/40 cursor-pointer h-10 mt-5 font-bold hover:bg-black/50 hover:text-white/90">Enter</button>
+                    <button type="submit" className="w-full rounded-lg bg-[rgba(227,197,215)] text-black cursor-pointer h-10 mt-5 font-bold hover:bg-[rgba(227,197,215,0.8)] hover:text-black/60 ">Enter</button>
                 </div>
                 <div className="flex justify-center">
                     <p>Don't have an account? <Link to="/register" className="font-bold hover:underline">Register</Link></p>
