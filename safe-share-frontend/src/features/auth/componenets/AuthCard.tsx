a56f59110904e172eaeabbcde01a25b6ca8bd758
Michal Kosiec <michalkosiecg@gmail.com>
2026-09-17 22:48:28 +0200

    Restore styles

--- safe-share-frontend/src/features/auth/componenets/AuthCard.tsx
+++ safe-share-frontend/src/features/auth/componenets/AuthCard.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 export default function AuthCard({title, children}: Props) {
     return (
-        <div className="flex flex-col w-2/5 p-6 bg-black/60 rounded-xl backdrop-blur-xl border border-black/10 shadow-2xl text-white max-w-2xl">
+        <div className="flex flex-col w-2/5 text-white max-w-2xl bg-[rgb(28,28,28)] rounded-2xl border border-white/10 p-5 shadow-lg backdrop-blur-sm">
             <div>
                 <h1 className="text-3xl font-bold tracking-wider">{title}</h1>
             </div>
